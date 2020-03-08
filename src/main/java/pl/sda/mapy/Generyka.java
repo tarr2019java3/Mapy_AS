28e2606863c77ee38c3203b78484892b19653664
Adam <ajjstepien@gmail.com>
2020-03-08 10:58:19 +0100

    generyki

---
+++ src/main/java/pl/sda/mapy/Generyka.java
@@ -0,0 +1,13 @@
+package pl.sda.mapy;
+
+public class Generyka<T> {
+    private T field;
+
+    public Generyka(T field) {
+        this.field = field;
+    }
+
+    public T getField() {
+        return field;
+    }
+}
--- src/main/java/pl/sda/mapy/Main.java
+++ src/main/java/pl/sda/mapy/Main.java
@@ -1,8 +1,16 @@
 package pl.sda.mapy;
 
+import java.util.List;
+import java.util.Map;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        Generyka<String> generyk1 = new Generyka<>("String");
+        System.out.println(generyk1.getField());
+
+        Generyka<Integer> generyk2 = new Generyka<>(15);
+        System.out.println(generyk2.getField());
     }
 }
